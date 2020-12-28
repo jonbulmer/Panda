3bda8191fe837358fe39cf60f8a308de266ac15b
Jon Bulmer <jon.bulmer@gmail.com>
2020-12-28 15:05:10 +0000

    First Commit for the Data API
    This is the first commit with a dummy API

---
+++ Panda.API.Service/Controllers/Global.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Panda.API.Service.Controllers
+{
+    [Route("panda.data/global")]
+    [ApiController]
+    public class GlobalController : ControllerBase
+    {
+        [HttpGet("confirmed")]
+        public IActionResult Confirmed(string date_from, string date_to, 
+            string contient, string region, string country)
+        {
+
+            return Ok("{ confirmed : 123, date_from : " + date_from + ", date_to : 26 Dec 2020 }"); 
+        }
+    }
+}
