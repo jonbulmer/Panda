7ad3ef6ea68c3122f15391d21fc31e865553183e
Jon Bulmer <jon.bulmer@gmail.com>
2021-01-01 20:28:03 +0000

    Just a start to the Public Data reader project
    Start work on the public data eader project All I have at the moment os to read AFG data into a json in memory object.

---
+++ Panda.Public.Data.Reader/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Panda.Public.Data.Reader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton<IPublicDataReader, PublicDataReader>()
+                .AddSingleton<IPublicJsonDataReader, PublicJsonDataReader>()
+                .BuildServiceProvider();
+
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            //do the actual work here
+            var json = serviceProvider.GetService<IPublicDataReader>();
+            json.ReadData();
+
+            logger.LogDebug("All done!");
+        }
+    }
+
+
+    public interface IPublicDataReader
+    {
+        void ReadData();
+    }
+
+    public interface IPublicJsonDataReader
+    {
+        public string ReadJson(string country);
+    }
+
+    public class PublicDataReader : IPublicDataReader
+    {
+        private readonly IPublicJsonDataReader _publicJsonDataReader;
+        public PublicDataReader(IPublicJsonDataReader publicJsonDataReader)
+        {
+            _publicJsonDataReader = publicJsonDataReader;
+        }
+        public void ReadData()
+        {
+            string thisJson = _publicJsonDataReader.ReadJson("ALB");
+        }
+    }
+
+    public class PublicJsonDataReader : IPublicJsonDataReader
+    {
+
+        public string ReadJson(string county)
+        {
+            string readJson = "";
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.json");
+            StringBuilder sb = new StringBuilder();
+            using (var reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    readJson = reader.ReadLine();
+                    readJson = readJson.Replace("\"", "'");
+                    sb.Append(readJson);
+                    if (readJson ==  "    '"+ county +"': {")
+                    {
+                        sb.Replace("    '" + county + "': {", "}");
+                        readJson = sb.ToString();
+                        return readJson;
+                    }
+                }
+            }
+            readJson = sb.ToString();
+            return readJson;
+        }
+    }
+}
