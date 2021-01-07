21c969477bce2cbf8616960d7e8a7fe73b568030
Jon Bulmer <jon.bulmer@gmail.com>
2021-01-07 21:17:23 +0000

    Added the ConnectionFactory interface

---
+++ Panda.Public.Data.Reader/ConnectionFactory/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Public.Data.Reader
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+        void CloseConnection();
+    }
+}
